3a5772e554c4d950d4d8f1a1d9182e168e3bdcc0
liams <princewill835@gmail.com>
2022-11-26 05:39:47 -0800

    fixed mobile responsiveness by making overflow hidden

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
 										className="cursor-pointer text-2xl"
 									/>
 								) : (
-									<IconContext.Provider value={{ color: "white"}}>
+									<IconContext.Provider value={{ color: "white" }}>
 										<BsFillSunFill
 											onClick={() => setDarkMode(!darkMode)}
 											className="cursor-pointer text-2xl"
@@ -179,18 +179,22 @@ export default function Home() {
 					</div>
 					<div className="relative flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
 						<div className="relative basis-1/3 flex-1 cursor-pointer hover:-translate-y-4 hover:ease-in hover:delay-150 hover:drop-shadow-[10px_5px_5px_rgb(0,0,0)]">
-							<div className="inset-0 z-10 bg-amber-400 text-left flex flex-col items-start justify-start opacity-0 hover:opacity-100 rounded-lg bg-opacity-100 duration-300 h-54 absolute">
-								<p className="text-sm py-6 px-6">React, SCSS, HTML, Git</p>
-								<p className="text-sm py-2 px-6">
+							<div className="inset-0 z-10 overflow-hidden bg-amber-400 text-left flex flex-col items-start justify-start opacity-0 hover:opacity-100 rounded-lg bg-opacity-100 duration-300 h-54 absolute">
+								<p className="text-sm py-4 px-6 lg:text-base">
+									React, SCSS, HTML, Git
+								</p>
+								<p className="text-sm py-2 px-6 lg:text-base">
 									A card game built to check one's retentiveness, I added audio
 									and animation to the game to give it a more stimulating effect
 								</p>
-								<a href="https://lambocard.netlify.app/">
-									<FaLink className="absolute bottom-2 right-14 h-10 w-10" />
-								</a>
-								<a href="https://github.com/IamLiam09/Memory-card">
-									<FaFileCode className="absolute bottom-2 right-2 h-10 w-10" />
-								</a>
+								<div className="absolute bottom-2 flex right-2 gap-1">
+									<a href="https://lambocard.netlify.app/">
+										<FaLink className="lg:h-10 lg:w-10 md:h-6 md:w-6" />
+									</a>
+									<a href="https://github.com/IamLiam09/Memory-card">
+										<FaFileCode className="lg:h-10 lg:w-10 md:h-6 md:w-6" />
+									</a>
+								</div>
 							</div>
 							<div className="h-54 flex flex-wrap content-center lg:h-54">
 								<Image
@@ -203,20 +207,22 @@ export default function Home() {
 							</div>
 						</div>
 						<div className="relative basis-1/3 cursor-pointer flex-1 hover:-translate-y-4 hover:ease-in hover:delay-150 hover:drop-shadow-[10px_5px_5px_rgb(0,0,0)]">
-							<div className="inset-0 z-10 bg-amber-400 text-left flex flex-col items-start justify-start opacity-0 hover:opacity-100 rounded-lg bg-opacity-100 duration-300 h-54 absolute">
-								<p className="text-sm py-6 px-6">
+							<div className="inset-0 z-10 overflow-hidden bg-amber-400 text-left flex flex-col items-start justify-start opacity-0 hover:opacity-100 rounded-lg bg-opacity-100 duration-300 h-54 absolute">
+								<p className="text-sm py-2 px-6 lg:text-base">
 									React, Bootstrap, HTML, Git
 								</p>
-								<p className="text-sm py-2 px-6">
+								<p className="text-sm py-2 px-6 lg:text-base">
 									Website that builds resumes. Asks for all the required details
 									needed to build one
 								</p>
-								<a href="https://cvbywill.netlify.app/">
-									<FaLink className="absolute bottom-2 right-14 h-10 w-10" />
-								</a>
-								<a href="https://github.com/IamLiam09/cv-project">
-									<FaFileCode className="absolute bottom-2 right-2 h-10 w-10" />
-								</a>
+								<div className="absolute bottom-2 flex right-2 gap-1">
+									<a href="https://cvbywill.netlify.app/">
+										<FaLink className="lg:h-10 lg:w-10 md:h-6 md:w-6" />
+									</a>
+									<a href="https://github.com/IamLiam09/cv-project">
+										<FaFileCode className="lg:h-10 lg:w-10 md:h-6 md:w-6" />
+									</a>
+								</div>
 							</div>
 							<div className="h-54 flex flex-wrap content-center lg:h-54">
 								<Image
@@ -229,21 +235,23 @@ export default function Home() {
 							</div>
 						</div>
 						<div className="relative basis-1/3 cursor-pointer flex-1 hover:-translate-y-4 hover:ease-in hover:delay-150 hover:drop-shadow-[10px_5px_5px_rgb(0,0,0)]">
-							<div className="inset-0 z-10 bg-amber-400 text-left flex flex-col items-start justify-start opacity-0 hover:opacity-100 rounded-lg bg-opacity-100 duration-300 h-54 absolute">
-								<p className="text-sm py-6 px-6">
+							<div className="inset-0 z-10 overflow-hidden bg-amber-400 text-left flex flex-col items-start justify-start opacity-0 hover:opacity-100 rounded-lg bg-opacity-100 duration-300 h-54 absolute">
+								<p className="text-sm py-2 px-6 lg:text-base">
 									Django, Bootstrap, SQL, Python, HTML, Git
 								</p>
-								<p className="text-sm py-2 px-6">
+								<p className="text-sm py-2 px-6 lg:text-base">
 									Blog site used to track your learning on a daily basis. User
 									authencation, database, api and alot more features were
 									implemented here. Fullstack app
 								</p>
-								<a href="blog-min.herokuapp.com">
-									<FaLink className="absolute bottom-2 right-14 h-10 w-10" />
-								</a>
-								<a href="https://github.com/IamLiam09/learning_log">
-									<FaFileCode className="absolute bottom-2 right-2 h-10 w-10" />
-								</a>
+								<div className="absolute bottom-2 flex right-2 gap-1">
+									<a href="blog-min.herokuapp.com">
+										<FaLink className="lg:h-10 lg:w-10 md:h-6 md:w-6" />
+									</a>
+									<a href="https://github.com/IamLiam09/learning_log">
+										<FaFileCode className="lg:h-10 lg:w-10 md:h-6 md:w-6" />
+									</a>
+								</div>
 							</div>
 							<div className="h-54 flex flex-wrap content-center lg:h-54">
 								<Image
@@ -256,19 +264,21 @@ export default function Home() {
 							</div>
 						</div>
 						<div className="relative basis-1/3 cursor-pointer flex-1 hover:-translate-y-4 hover:ease-in hover:delay-150 hover:drop-shadow-[10px_5px_5px_rgb(0,0,0)]">
-							<div className="inset-0 z-10 bg-amber-400 text-sm text-left flex flex-col items-start justify-start opacity-0 hover:opacity-100 rounded-lg bg-opacity-100 duration-300 h-54 absolute ">
-								<p className="text-sm py-6 px-6">
+							<div className="inset-0 z-10 overflow-hidden bg-amber-400 text-sm text-left flex flex-col items-start justify-start opacity-0 hover:opacity-100 rounded-lg bg-opacity-100 duration-300 h-54 absolute ">
+								<p className="text-sm py-2 px-6 lg:text-base">
 									JavaScript, SCSS, HTML, Git
 								</p>
-								<p className="text-sm py-2 px-6">
+								<p className="text-sm py-2 px-6 lg:text-base">
 									Responsive restaurant page with feature like swipe for mobile.
 								</p>
-								<a href="https://afrirokitchen.netlify.app/">
-									<FaLink className="absolute bottom-2 right-14 h-10 w-10" />
-								</a>
-								<a href="https://github.com/IamLiam09/Restaurant">
-									<FaFileCode className="absolute bottom-2 right-2 h-10 w-10" />
-								</a>
+								<div className="absolute bottom-2 flex right-2 gap-1">
+									<a href="https://afrirokitchen.netlify.app/">
+										<FaLink className="lg:h-10 lg:w-10 md:h-6 md:w-6" />
+									</a>
+									<a href="https://github.com/IamLiam09/Restaurant">
+										<FaFileCode className="lg:h-10 lg:w-10 md:h-6 md:w-6" />
+									</a>
+								</div>
 							</div>
 							<div className="h-54 flex flex-wrap content-center lg:h-54">
 								<Image
